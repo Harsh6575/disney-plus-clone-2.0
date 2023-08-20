7ca11bc0760d7ab545fd55634d9133f2caf1ed10
Harsh6575 <harsh.190410107146@gmail.com>
2023-08-20 15:02:39 +0530

    solving error of dataset not found

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import movies from "../dataset.json";
-
 const Details = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState({
